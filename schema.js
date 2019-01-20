7b319ea1cb3bc650d6361ea5a957fb986b07b911
Akshit Gupta <akshitgupta1996@gmail.com>
2019-01-20 08:00:53 -0800

    added launch type

--- schema.js
+++ schema.js
@@ -24,7 +24,7 @@ const LaunchType = new GraphQLObjectType({
 });
 
 // Rocket Type
-const LaunchType = new GraphQLObjectType({
+const RocketType = new GraphQLObjectType({
     name: 'Rocket',
     fields: () => ({
         rocket_id : {type: GraphQLString},
@@ -39,10 +39,20 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         launches: {
-            type: new GraphQLList({LaunchType}),
+            type: new GraphQLList(LaunchType),
             resolve(parent, args){
                 return axios.get('https://api.spacexdata.com/v3/launches')
                 .then(res => res.data);
+            }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: {type: GraphQLInt}
+            },
+            resolve(parent, args){
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                .then(res => res.data)
             }
         }
     }
