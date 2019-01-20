e8c6ef78e997b25fb2142dd41769707063c509f3
Akshit Gupta <akshitgupta1996@gmail.com>
2019-01-20 07:45:29 -0800

    added root query

--- schema.js
+++ schema.js
@@ -34,3 +34,20 @@ const LaunchType = new GraphQLObjectType({
 });
  
 
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        launches: {
+            type: new GraphQLList({LaunchType}),
+            resolve(parent, args){
+                return axios.get('https://api.spacexdata.com/v3/launches')
+                .then(res => res.data);
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
