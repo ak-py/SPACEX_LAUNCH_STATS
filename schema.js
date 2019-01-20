d5b77c7544a26e0a373fdd4f28986fe8597278d6
Akshit Gupta <akshitgupta1996@gmail.com>
2019-01-20 07:39:35 -0800

    added schema with Launch and Rocket type objects

---
+++ schema.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLBoolean,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+// Launch Type
+const LaunchType = new GraphQLObjectType({
+    name: 'Launch',
+    fields: () => ({
+        flight_number : {type: GraphQLInt},
+        mission_name: {type: GraphQLString},
+        launch_year : {type: GraphQLString},
+        launch_date_local: {type: GraphQLString},
+        launch_success : {type: GraphQLBoolean},
+        rocket: {type: RocketType}
+    })
+});
+
+// Rocket Type
+const LaunchType = new GraphQLObjectType({
+    name: 'Rocket',
+    fields: () => ({
+        rocket_id : {type: GraphQLString},
+        rocket_name: {type: GraphQLString},
+        rocket_type : {type: GraphQLString}
+    })
+});
+ 
+
